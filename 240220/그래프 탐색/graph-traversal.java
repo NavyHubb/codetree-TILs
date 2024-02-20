23f685467c2932000574e65d3296eef6c67352a5
Navy <101810007+NavyHubb@users.noreply.github.com>
2024-02-20 09:07:25 +0900

    Title: 그래프 탐색, Time: 261ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240220/그래프 탐색/graph-traversal.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static List<List<Integer>> graph;
+    static boolean[] visited;
+    static int cnt;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+        visited = new boolean[N+1];
+        visited[1] = true;
+        cnt = 0;
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        DFS(1);
+
+        System.out.print(cnt);
+    }
+
+    public static void DFS(int n) {
+        for (int i : graph.get(n)) {
+            if (visited[i]) continue;
+
+            visited[i] = true;
+            cnt++;
+            DFS(i);
+        }
+    }
+
+}
