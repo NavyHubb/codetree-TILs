a430b56aeb61fa897ba20b22fa152b7e941ec09f
Navy <101810007+NavyHubb@users.noreply.github.com>
2024-02-24 20:31:59 +0900

    Title: 사각형 채우기 3, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240224/사각형 채우기 3/rectangle-fill-3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static final int MOD = 10007;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] dp = new int[1001];
+        dp[0] = 1;
+        dp[1] = 2;
+        dp[2] = 7;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i-1]*2 + dp[i-2]*4) % MOD;
+        }
+
+        System.out.println(dp[N]);
+    }
+
+}
