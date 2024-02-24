a3e5f699cc266422a81de9a6d52c26d6575774ed
Navy <101810007+NavyHubb@users.noreply.github.com>
2024-02-24 20:55:14 +0900

    Title: 사각형 채우기 3, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240224/사각형 채우기 3/rectangle-fill-3.java
+++ 240224/사각형 채우기 3/rectangle-fill-3.java
@@ -8,7 +8,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        long[] dp = new long[1001];
+        long[] dp = new long[1001];  // MOD를 두배 한것과 세배 한것을 더하면 int형의 범위를 초과할 수 있으므로 long으로 선언
         dp[0] = 1;
         dp[1] = 2;
 
