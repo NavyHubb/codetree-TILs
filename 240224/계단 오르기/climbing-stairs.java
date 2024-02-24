d7c69e5a69565c767dee0f5b30ddf4534aff3ca0
Navy <101810007+NavyHubb@users.noreply.github.com>
2024-02-24 18:23:35 +0900

    Title: 계단 오르기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240224/계단 오르기/climbing-stairs.java
+++ 240224/계단 오르기/climbing-stairs.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Main {
 
+    static final int MOD = 10007; 
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
@@ -10,10 +12,10 @@ public class Main {
         dp[3] = dp[2] = 1;
 
         for (int i = 4; i <= N; i++) {
-            dp[i] = dp[i-2] + dp[i-3];
+            dp[i] = dp[i-2]%MOD + dp[i-3]%MOD;
         }
 
         System.out.println(dp[N]);
     }
-    
+
 }
