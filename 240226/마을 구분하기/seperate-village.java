0525c31a105abe4860ea9e39e76af520d5af3f8d
Navy <101810007+NavyHubb@users.noreply.github.com>
2024-02-26 23:49:03 +0900

    Title: 마을 구분하기, Time: 281ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240226/마을 구분하기/seperate-village.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Main {
+
+    static int N;
+    static int[][] map;
+    static boolean[][] visited;
+    static PriorityQueue<Integer> pq;
+    static int[] di = {-1, 1, 0, 0};
+    static int[] dj = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][];
+        for (int i = 0; i < N; i++) {
+            map[i] = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt).toArray();
+        }
+        visited = new boolean[N][N];
+        pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 0) continue;
+                if (visited[i][j]) continue;
+
+                BFS(i, j);
+            }
+        }
+
+        System.out.println(pq.size());
+        while (!pq.isEmpty()) {
+            System.out.println(pq.poll());
+        }
+    }
+
+    static void BFS(int si, int sj) {
+        Queue<int[]> que = new ArrayDeque<>();
+
+        que.add(new int[]{si, sj});
+        visited[si][sj] = true;
+
+        int cnt = 0;
+        while (!que.isEmpty()) {
+            int[] cur = que.poll();
+            int i = cur[0], j = cur[1];
+            cnt++;
+
+            for (int d = 0; d < 4; d++) {
+                int ni = i + di[d];
+                int nj = j + dj[d];
+
+                if (ni < 0 || nj < 0 || ni >= N || nj >= N) continue;
+                if (map[ni][nj] == 0) continue;
+                if (visited[ni][nj]) continue;
+
+                visited[ni][nj] = true;
+                que.add(new int[]{ni, nj});
+            }
+        }
+
+        pq.add(cnt);
+    }
+
+}
