143b46e39af40fea8df50628a81d19963765addf
Navy <101810007+NavyHubb@users.noreply.github.com>
2024-03-02 16:05:01 +0900

    Title: 아름다운 수, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240302/아름다운 수/beautiful-number.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int N;
+    static int cnt = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        solution(0);
+        System.out.print(cnt);
+    }
+
+    public static void solution(int depth) {
+        if (depth == N) {
+            cnt++;
+            return;
+        }
+
+        for (int i = 1; i <= 9; i++) {
+            if (depth + i > N) break;
+
+            solution(depth+i);
+        }
+    }
+}
