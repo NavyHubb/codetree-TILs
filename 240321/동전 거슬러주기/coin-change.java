359b99c18892fa9c476621d5ee6f804d56c9b987
Navy <101810007+NavyHubb@users.noreply.github.com>
2024-03-21 00:32:17 +0900

    Title: 동전 거슬러주기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240321/동전 거슬러주기/coin-change.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    static final int INF = Integer.MAX_VALUE;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] coins = new int[N];
+        for (int i = 0; i < N; i++) {
+            coins[i] = sc.nextInt();
+        }
+        Arrays.sort(coins);  // 오름차순 정렬
+
+        int[] dp = new int[M+1];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+
+        Loop: for (int i = 1; i <= M; i++) {
+            for (int coin : coins) {
+                if (i < coin) continue Loop;
+
+                dp[i] = Math.min(dp[i], dp[i-coin]+1);
+            }
+        }
+
+        System.out.println(dp[M] == INF ? -1 : dp[M]);
+    }
+}
