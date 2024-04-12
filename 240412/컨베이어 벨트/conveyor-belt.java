0e4a702262e5e59ac03c512a949919854187d9f1
Navy <101810007+NavyHubb@users.noreply.github.com>
2024-04-12 20:29:28 +0900

    Title: 컨베이어 벨트, Time: 290ms, Memory: 14MB, Status: Passed - CodeTree

--- 240412/컨베이어 벨트/conveyor-belt.java
+++ 240412/컨베이어 벨트/conveyor-belt.java
@@ -15,27 +15,32 @@ public class Main {
                 .mapToInt(Integer::parseInt).toArray();
         }
 
-        // 1. 첫째 줄 가장 우측 수 저장
-        int first = arr[0][N-1];
+        T %= 2*N;
+        for (int t = 0; t < T; t++) {
+            // 1. 첫째 줄 가장 우측 수 저장
+            int first = arr[0][N-1];
 
-        // 2. 첫째 줄 우로 한칸씩 이동
-        for (int j = N-1; j >= 1; j--) {
-            arr[0][j] = arr[0][j-1];
+            // 2. 첫째 줄 우로 한칸씩 이동
+            for (int j = N-1; j >= 1; j--) {
+                arr[0][j] = arr[0][j-1];
+            }
+
+            // 3. 둘째 줄 가장 우측 수 저장
+            int second = arr[1][N-1];  
+
+            // 4. 둘째 줄 우로 한칸씩 이동
+            for (int j = N-1; j >= 1; j--) {
+                arr[1][j] = arr[1][j-1];
+            }
+
+            // 5. 1에서 저장한 수 둘째 줄 좌측에 대입
+            arr[1][0] = first;
+
+            // 6. 3에서 저장한 수 첫째 줄 좌측에 대입
+            arr[0][0] = second;
         }
-
-        // 3. 둘째 줄 가장 우측 수 저장
-        int second = arr[1][N-1];  
-
-        // 4. 둘째 줄 우로 한칸씩 이동
-        for (int j = N-1; j >= 1; j--) {
-            arr[1][j] = arr[1][j-1];
-        }
-
-        // 5. 1에서 저장한 수 둘째 줄 좌측에 대입
-        arr[1][0] = first;
-
-        // 6. 3에서 저장한 수 첫째 줄 좌측에 대입
-        arr[0][0] = second;
+        
+        
      
 
         StringBuilder sb = new StringBuilder();
