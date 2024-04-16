5c27807bdcfe974712d20bdc37f90d12fe0c3fe5
Navy <101810007+NavyHubb@users.noreply.github.com>
2024-04-16 23:05:35 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 3040ms, Memory: 84MB, Status: Time Limit Exceed - CodeTree

---
+++ 240416/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N, max;
+    static int[][] map;
+    static int[][] dp;
+    static boolean[][] visited;
+    static int[] di = {-1, 1, 0, 0};
+    static int[] dj = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][];
+        for (int i = 0; i < N; i++) {
+            map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        dp = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                visited = new boolean[N][N];
+                max = 0;
+                dfs(i, j, 1);
+
+                dp[i][j] = max;
+                result = Math.max(result, dp[i][j]);
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    public static void dfs(int i, int j, int cnt) {
+        max = Math.max(max, cnt);
+
+        for (int d = 0; d < 4; d++) {
+            int ni = i + di[d];
+            int nj = j + dj[d];
+
+            if (ni < 0 || nj < 0 || ni >= N || nj >= N) continue;
+
+            if (map[ni][nj] > map[i][j]) {
+                dfs(ni, nj, cnt+1);
+            }
+        }
+    }
+
+}
