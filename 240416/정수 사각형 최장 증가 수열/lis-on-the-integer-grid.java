1594d07d4963f610f30a63ee0a00e4af4026aa80
Navy <101810007+NavyHubb@users.noreply.github.com>
2024-04-16 23:45:01 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 882ms, Memory: 45MB, Status: Passed - CodeTree

--- 240416/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 240416/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -3,54 +3,52 @@ import java.io.*;
 
 public class Main {
 
-    static int N, max;
+    static int N, result;
     static int[][] map;
     static int[][] dp;
-    static boolean[][] visited;
     static int[] di = {-1, 1, 0, 0};
     static int[] dj = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
         N = Integer.parseInt(br.readLine());
-        map = new int[N][];
+        map = new int[N][N];
+        dp = new int[N][N];
         for (int i = 0; i < N; i++) {
-            map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            Arrays.fill(dp[i], 1);
         }
 
-        dp = new int[N][N];
-
-        int result = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
         for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                max = 0;
-                dfs(i, j, 1);
-
-                dp[i][j] = max;
-                result = Math.max(result, dp[i][j]);
+                map[i][j] = Integer.parseInt(st.nextToken());
+                pq.offer(new int[]{i, j, map[i][j]});
             }
+        }
+
+        result = 0;
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int i = cur[0], j = cur[1], num = cur[2];
+
+            for (int d = 0; d < 4; d++) {
+                int ni = i + di[d];
+                int nj = j + dj[d];
+
+                if (ni < 0 || nj < 0 || ni >= N || nj >= N) continue;
+
+                if (map[ni][nj] > num) {
+                    dp[ni][nj] = Math.max(dp[ni][nj], dp[i][j] + 1);
+                }
+            }
+
+            result = Math.max(result, dp[i][j]);
         }
 
         System.out.println(result);
-    }
-
-    public static void dfs(int i, int j, int cnt) {
-        max = Math.max(max, cnt);
-
-        for (int d = 0; d < 4; d++) {
-            int ni = i + di[d];
-            int nj = j + dj[d];
-
-            if (ni < 0 || nj < 0 || ni >= N || nj >= N) continue;
-
-            if (map[ni][nj] > map[i][j]) {
-                if (dp[ni][nj] == 0) {
-                    dfs(ni, nj, cnt+1);
-                } else {
-                    max = Math.max(max, cnt+dp[ni][nj]);
-                }
-            }
-        }
     }
 
 }
