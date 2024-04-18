3a26175aa825dfeafe05869fb551ceca1686a44d
Navy <101810007+NavyHubb@users.noreply.github.com>
2024-04-18 22:17:41 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 386ms, Memory: 19MB, Status: Passed - CodeTree

---
+++ 240418/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// 최솟값 이상의 수들만 써서 이동한다는 가정하에 경로상에 놓여있는 수들 중 최댓값을 최소화하는 전략 사용
+public class Main {
+
+    static final int INF = Integer.MAX_VALUE;
+    static int N;
+    static int[][] map, dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][];
+        for (int i = 0; i < N; i++) {
+            map[i] = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        }
+
+        int result = INF;
+        for (int i = 1; i <= 100; i++) {
+            int upperBound = solve(i);
+
+            result = Math.min(upperBound - i, result);
+        }
+
+        System.out.println(result);
+    }
+    
+    public static int solve(int lowerBound) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] < lowerBound) {
+                    map[i][j] = INF;
+                }
+            }
+        }
+
+        init();
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < N; j++) {
+                dp[i][j] = Math.max(Math.min(dp[i-1][j], dp[i][j-1]), map[i][j]);
+            }
+        }
+
+        return dp[N-1][N-1];
+    }
+
+    public static void init() {
+        dp = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                dp[i][j] = INF;
+            }
+        }
+
+        dp[0][0] = map[0][0];
+        for (int k = 1; k < N; k++) {
+            dp[k][0] = Math.max(dp[k-1][0], map[k][0]);
+            dp[0][k] = Math.max(dp[0][k-1], map[0][k]);
+        }
+    }
+}
